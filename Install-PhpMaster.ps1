452ee07451680d20cb1bc0ac69ddae5356c0095d
Shivam Mathur <shivam_jpr@hotmail.com>
2020-07-24 16:35:25 +0530

    Replace version with master

---
+++ Install-PhpMaster.ps1
@@ -0,0 +1,42 @@
+<#
+.Synopsis
+Installs PHP nightly.
+.Description
+Download and installs a nightly version of PHP.
+.Parameter Architecture
+The architecture of the PHP to be installed (x86 for 32-bit, x64 for 64-bit).
+.Parameter ThreadSafe
+A boolean value to indicate if the Thread-Safe version should be installed or not.
+You usually install the ThreadSafe version if you plan to use PHP with Apache, or the NonThreadSafe version if you'll use PHP in CGI mode.
+.Parameter Path
+The path of the directory where PHP will be installed.
+ #>
+param (
+  [Parameter(Mandatory = $true, Position = 1, HelpMessage = 'Architecture of the PHP to be installed (x86 for 32-bit, x64 for 64-bit)')]
+  [ValidateSet('x86', 'x64')]
+  [string] $Architecture,
+  [Parameter(Mandatory = $true, Position = 2, HelpMessage = 'Install a Thread-Safe version?')]
+  [bool] $ThreadSafe,
+  [Parameter(Mandatory = $true, Position = 3, HelpMessage = 'The path of the directory where PHP will be installed')]
+  [ValidateLength(1, [int]::MaxValue)]
+  [string] $Path
+)
+if(-not(Test-Path $Path)) {
+  New-Item -Type 'directory' $Path
+}
+$ts = 'nts'
+if($ThreadSafe) {
+  $ts = 'ts'
+}
+Invoke-WebRequest -UseBasicParsing -Uri "https://dl.bintray.com/shivammathur/php/php-master-$ts-windows-vs16-$Architecture.zip" -OutFile $Path\master.zip
+Expand-Archive -Path $Path\master.zip -DestinationPath $Path -Force
+Copy-Item $Path\php.ini-production -Destination $Path\php.ini
+"xdebug", "pcov" | ForEach-Object { Invoke-WebRequest -UseBasicParsing -Uri "https://github.com/shivammathur/php-extensions-windows/releases/latest/download/php_$ts`_$Architecture`_$_.dll" -OutFile $Path"\ext\php`_$_.dll" }
+$ini_content = @"
+extension_dir=$Path\ext
+default_charset=UTF-8
+zend_extension=php_opcache.dll
+opcache.jit_buffer_size=256M
+opcache.jit=1235
+"@
+Add-Content -Path $Path\php.ini -Value $ini_content
--- README.md
+++ README.md
@@ -2,9 +2,16 @@
 
 <a href="https://github.com/shivammathur/php-builder-windows" title="PHP Builder Windows"><img alt="Build status" src="https://github.com/shivammathur/php-builder-windows/workflows/Build%20PHP/badge.svg"></a>
 <a href="https://github.com/shivammathur/php-builder-windows/blob/main/LICENSE" title="license"><img alt="LICENSE" src="https://img.shields.io/badge/license-MIT-428f7e.svg"></a>
-<a href="https://github.com/shivammathur/php-builder-windows" title="builds"><img alt="PHP Versions Supported" src="https://img.shields.io/badge/php-%3E%3D%208.0-8892BF.svg"></a>
+<a href="https://github.com/shivammathur/php-builder-windows#Builds" title="builds"><img alt="PHP Versions Supported" src="https://img.shields.io/badge/php-%3E%3D%208.0-8892BF.svg"></a>
 
 > Build PHP nightly for windows.
+
+## Builds
+- [php-master-nts-windows-vs16-x64.zi](https://bintray.com/shivammathur/php/php-master-nts-windows-vs16-x64.zip)
+- [php-master-nts-windows-vs16-x64-AVX.zip](https://bintray.com/shivammathur/php/php-master-ts-windows-vs16-x64-AVX.zip)
+- [php-master-ts-windows-vs16-x64.zip](https://bintray.com/shivammathur/php/php-master-ts-windows-vs16-x64.zip)
+- [php-master-nts-windows-vs16-x86.zip](https://bintray.com/shivammathur/php/php-master-nts-windows-vs16-x86.zip)
+- [php-master-ts-windows-vs16-x86.zip](https://bintray.com/shivammathur/php/php-master-ts-windows-vs16-x86.zip)
 
 ## License
 
