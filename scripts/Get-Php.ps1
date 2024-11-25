3452b4e1d93773b6fc4dc20d04f53095f38347f1
Shivam Mathur <shivam_jpr@hotmail.com>
2024-11-25 20:42:32 +0000

    Mark PHP 8.4 as default in scripts

--- scripts/Get-Php.ps1
+++ scripts/Get-Php.ps1
@@ -24,7 +24,7 @@ param (
   [string] $Path,
   [Parameter(Mandatory = $false, Position = 4, HelpMessage = 'The PHP version')]
   [ValidateLength(1, [int]::MaxValue)]
-  [string] $Version = '8.3'
+  [string] $Version = '8.4'
 )
 
 Function Get-File {
