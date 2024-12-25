6f9f6e188d58fb1859abe98ee6d7b1619ad537af
Shivam Mathur <shivam_jpr@hotmail.com>
2024-12-25 04:51:02 +0000

    Update trunk in Update fetch.sh [skip ci]

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -2,11 +2,11 @@ version=$1
 directory=$2
 (
     cd "$directory" || exit 1
-    trunk=https://windows.php.net/downloads/releases
+    trunk=https://downloads.php.net/~windows/releases
     semver=$(curl -sL "$trunk"/releases.json | jq -r ".[\"$version\"].version")
     if [ "$version" != "${semver%.*}" ]; then
-        semver=$(curl -sL "$trunk"/archives/ | grep -Po '(?<=HREF=")[^"]*' | grep -Po "$version.[0-9]+" | sort -V | tail -1)
+        semver=$(curl -sL "$trunk"/archives/ | grep -Po '(?<=href=")[^"]*' | grep -Po "$version.[0-9]+" | sort -V | tail -1)
         trunk="$trunk"/archives
     fi
-    curl -sL "$trunk" | grep -Po '(?<=HREF=")[^"]*' | grep -E "$semver" | xargs -n 1 -I{} curl -sLO https://windows.php.net/{}
+    curl -sL "$trunk" | grep -Po '(?<=href=")[^"]*' | grep -E "$semver" | xargs -n 1 -I{} curl -sLO $trunk/{}
 )
